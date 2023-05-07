47b1925faf006aa0ef120ffe2b21a4616a49ab5e
wasertech <danny@waser.tech>
2023-05-07 11:08:24 +0200

    add evaluate for testing sets

---
+++ scorer/dataset/csv.py
@@ -0,0 +1,58 @@
+# Dataset Importer for DeepSpeech/STT CSV dataset format
+
+## wav_filename, wav_filesize, transcript
+
+import os
+from glob import glob
+from datasets import concatenate_datasets, load_dataset, DatasetDict
+from datasets.features import Audio
+
+
+def load_16khz_audio(dataset):
+    sr = 16000
+    audio_column_name = "wav_filename"
+    return dataset.cast_column(
+            audio_column_name, Audio(sampling_rate=sr)
+        )
+
+def load_train_dataset_csv(data_path):
+    pass
+
+def load_dev_dataset_csv(data_path):
+    pass
+
+def load_test_dataset_csv(data_path, max_eval_samples=None, text_column_name="transcript"):
+    test_files = glob(f"{str(data_path)}/**/*_dev.csv")
+    if not test_files:
+        raise ValueError(f"No test files found under {data_path}")
+    dataset_dict = DatasetDict()
+    for tf in test_files:
+        test_data = load_dataset('csv', data_files=[tf])
+        _dn = os.path.dirname(tf)
+        base_abs_path = os.path.abspath(_dn)
+
+        def get_absolute_wavpath(wavpath):
+            wfn = wavpath['wav_filename']
+            wfp = os.path.join(base_abs_path, wfn)
+            wavpath['wav_filename'] = wfp
+            return wavpath
+        
+        test_data = test_data.map(
+            get_absolute_wavpath, desc="Mapping relative wav files to absolute path"
+        )
+
+        if max_eval_samples is not None:
+            test_data["train"] = test_data["train"].select(range(max_eval_samples))
+
+        # Filter raw_datasets['eval'] to only include row with transcript not None
+        test_data["train"] = test_data["train"].filter(lambda row: row[text_column_name] not in [None, "", " ", "\n"])
+
+        test_data = test_data.rename_column("train", "test")
+
+        dataset_dict[f"{_dn.lower().replace(' ', '_')}"] = load_16khz_audio(test_data)
+    
+    return dataset_dict
+
+def load_dataset_csv(data_path):
+    ds = DatasetDict()
+    ds["test"] = load_test_dataset_csv(data_path)
---
+++ scorer/evaluate.py
@@ -0,0 +1,57 @@
+import torchaudio
+from datasets import load_dataset, load_metric
+from transformers import (
+    Wav2Vec2ForCTC,
+    Wav2Vec2Processor,
+)
+import evaluate
+import torch
+import re
+
+
+from scorer.dataset.csv import load_test_dataset_csv
+
+model_name = "wasertech/wav2vec2-cv-fr-9"
+device = "cuda"
+
+chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'  # noqa: W605
+
+model = Wav2Vec2ForCTC.from_pretrained(model_name).to(device)
+processor = Wav2Vec2Processor.from_pretrained(model_name)
+
+datasets = load_test_dataset_csv("/mnt/extracted/data")
+
+def map_to_pred(batch):
+    features = processor(batch["audio"], sampling_rate=16000, padding=True, return_tensors="pt")
+    input_values = features.input_values.to(device)
+    attention_mask = features.attention_mask.to(device)
+    with torch.no_grad():
+        logits = model(input_values, attention_mask=attention_mask).logits
+    pred_ids = torch.argmax(logits, dim=-1)
+    batch["predicted"] = processor.batch_decode(pred_ids)
+    batch["target"] = batch["transcript"].apply(lambda x: re.sub(chars_to_ignore_regex, "", x))
+    return batch
+
+
+def main():
+    wer = evaluate.load("wer")
+    cer = evaluate.load("cer")
+
+    for split, data in datasets.items():
+
+        print(f"Evalutating {split}...")
+
+        result = data.map(map_to_pred, batched=True, batch_size=16, remove_columns=list(data.features.keys()))
+
+        _w = wer.compute(predictions=result["predicted"], references=result["target"])
+        _c = cer.compute(predictions=result["predicted"], references=result["target"])
+        
+        print("-"*13)
+        print(f"|\t{split}\t|")
+        print("-"*13)
+        print("|\tWER\t|\tCER\t|")
+        print(f"|\t{_w['wer']:.2%}\t|\t{_c['cer']:.2%}\t|")
+        print("-"*13)
+
+if __name__ == "__main__":
+    main()
